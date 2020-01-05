7491723586e84fc48de01bb43eb0c1f6efede07b
codepleb <codepleb4@gmail.com>
2020-01-05 21:43:53 +0100

    Make hardlinks possible

--- router2.js
+++ router2.js
@@ -4,6 +4,12 @@ class Router {
       const newRoutingUrl = newURL.substring(newURL.indexOf("#") + 1);
       this.navigate(newRoutingUrl);
     };
+    // If the user routes to a specific URL from the beginning, it should load the appropriate page.
+    setTimeout(() => {
+      this.navigate(
+        window.location.href.substring(window.location.href.indexOf("#") + 1)
+      );
+    });
   }
 
   routes = [];
@@ -23,60 +29,12 @@ class Router {
     return this;
   };
 
-  //   flush = () => {
-  //     this.routes = [];
-  //     return this;
-  //   };
-
-  //   getFragment = () => {
-  //     let fragment = "";
-  //     if (this.mode === "history") {
-  //       //   fragment = this.clearSlashes(
-  //       //     decodeURI(window.location.pathname + window.location.search)
-  //       //   );
-  //       fragment = fragment.replace(/\?(.*)$/, ""); // Remove GET parameters from URL
-  //       fragment = this.root !== "/" ? fragment.replace(this.root, "") : fragment;
-  //     } else {
-  //       const match = window.location.href.match(/#(.*)$/); // Does URL contain a hash?
-  //       fragment = match ? match[1] : "";
-  //     }
-  //     return fragment.toString();
-  //   };
-
   navigate = (routingUrl = "") => {
-    // if (this.mode === "history") {
     window.history.pushState(null, null, "/#" + routingUrl);
-    this.routes.filter(r => r.path === routingUrl)[0].cb();
-    // window.location.href = window.location.href;
-    // } else {
-    //   window.location.href = `${window.location.href.replace(
-    //     /#(.*)$/,
-    //     ""
-    //   )}#${path}`; // Remove everything after # and then set the URL with a preceding hash
-    // }
+    const routes = this.routes.filter(r => r.path === routingUrl);
+    if (routes && routes[0]) routes[0].cb();
     return this;
   };
-
-  //   listen = () => {
-  //     clearInterval(this.interval);
-  //     this.interval = setInterval(this.interval, 50);
-  //   };
-
-  // Listen for path changes
-  //   interval = () => {
-  //     if (this.current === this.getFragment()) return;
-  //     this.current = this.getFragment();
-
-  //     this.routes.some(route => {
-  //       const match = this.current.match(route.path);
-  //       if (match) {
-  //         match.shift();
-  //         route.cb.apply({}, match);
-  //         return match;
-  //       }
-  //       return false;
-  //     });
-  //   };
 }
 
 export default Router;
