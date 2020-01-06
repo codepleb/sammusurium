09178c70c4f4d6d8daff455eaa9c1c962495651f
codepleb <codepleb4@gmail.com>
2020-01-06 13:53:47 +0100

    Init make navbar responsive

--- components/navbar.js
+++ components/navbar.js
@@ -8,12 +8,26 @@ class Navbar extends HTMLElement {
   connectedCallback() {
     let shadow = this.attachShadow({ mode: "open" });
     shadow.innerHTML = `
+    <style>
+        @media (min-width: 576px) {
+            button {
+                display: none;
+            }
+        }
+ 
+        @media (max-width: 576px) {
+            nav {
+                display: none;
+            }
+        }
+    </style>
     <nav>
         <cp-button-link href="/#/">Home</cp-button-link>
         <cp-button-link href="/#/login">Login</cp-button-link>
         <cp-button-link href="/#/random">Random</cp-button-link>
         <cp-button-link href="/#/about">About</cp-button-link>
     </nav>
+    <button id="">+</button>
   `;
   }
 }
