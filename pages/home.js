fdc8152f659a1e7b83d81f071d3ee451134d650d
codepleb <codepleb4@gmail.com>
2020-01-18 23:16:15 +0100

    Change homepage

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,29 @@
 export const homePage = `
+<style>
+    .content {
+        display: flex;
+        flex-direction: row;
+    }
+
+    .text {
+        align-self: center;
+    }
+
+    img {
+        width: 250px;
+        // height: 250px;
+        border-radius: 125px;
+        margin-right: 20px;
+    }
+</style>
+
 <h1>Welcome to codepleb's</h1>
-<p>
-    This is just a webpage. Several things I tried and tested just randomly appear here. No frameworks and libraries are being used, everything you see here are core web technologies.
-</p>
+
+<div class="content">
+    <img src="/assets/codepleb.png">
+    <div class="text">
+        <p>This is just a webpage. Several things I tried and tested just randomly appear here.</p>
+        <p>No frameworks and libraries are being used, everything you see here are core web technologies.</p>
+    </div>
+</div>
 `;
